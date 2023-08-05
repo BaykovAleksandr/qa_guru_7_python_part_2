bd75644be866e5ec54749a7fa0f700c02220d63b
BaykovAleksandr <baykov-1988@mail.ru>
2023-08-05 23:27:06 +0300

    "Причесал" локаторы покрасивее, сделал выбор даты кликами, а не вводом текста.

--- test_automation_practice_form.py
+++ test_automation_practice_form.py
@@ -1,4 +1,3 @@
-import time
 from selene import browser
 from selene import be, have
 
@@ -9,30 +8,19 @@ def test_positive(config_browser):
     browser.element('#lastName').should(be.blank).type('Baykov')
     browser.element('#userEmail').should(be.blank).type('test@yandex.com')
     browser.element('[for="gender-radio-1"]').click()
-    browser.element('[placeholder="Mobile Number"]').should(be.blank).type('1234567890')
-    browser.element('[id="dateOfBirth-wrapper"]').click()
-    browser.element('[class="react-datepicker__month-select"]').type('December')
-    browser.element('[class="react-datepicker__year-select"]').type('1988')
+    browser.element('#userNumber').should(be.blank).type('1234567890')
+    browser.element('#dateOfBirthInput').click()
+    browser.element(".react-datepicker__month-select>option[value='11']").click()
+    browser.element(".react-datepicker__year-select>option[value='1988']").click()
     browser.element('.react-datepicker__day--019').click()
     browser.element('#subjectsInput').should(be.blank).type('Maths').press_enter()
     browser.element("[for='hobbies-checkbox-3']").click()
     browser.element('#currentAddress').type("Moscow Region")
     browser.element('#react-select-3-input').type('ncr').press_enter()
     browser.element('#react-select-4-input').type('delhi').press_enter()
-    browser.element('[id=submit]').press_enter()
-    time.sleep(5)
+    browser.element('#submit').press_enter()
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-    time.sleep(5)
-    browser.element('.table').should(have.text('Aleksandr Baykov' and
-    'test@yandex.com' and
-    'Male' and
-    '1234567890' and
-    '19 December,1988' and
-    'Maths' and
-    'Music' and
-    'Moscow Region' and
-    'NCR Delhi'))
+    browser.element('.table').should(have.text('Aleksandr Baykov' and 'test@yandex.com' and 'Male' and
+                                               '1234567890' and '19 December,1988' and 'Maths' and 'Music' and
+                                               'Moscow Region' and 'NCR Delhi'))
     print("Выведенные на экран поля соответствуют заполненным")
-
-
-
